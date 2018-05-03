87163925516c812d95d2636de8dc34956881749d
jaamito <gm.2012.ian.lopez@gmail.com>
2018-05-03 17:09:01 +0200

    Apelligo->apellido

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,7 +10,7 @@
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
-
+                        <!--NOMBRE-->
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                             <label for="name" class="col-md-4 control-label">Name</label>
 
@@ -24,7 +24,21 @@
                                 @endif
                             </div>
                         </div>
+                        <!--NOMBRE-->
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-4 control-label">Name</label>
 
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <!--EMAIL-->
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
@@ -38,7 +52,7 @@
                                 @endif
                             </div>
                         </div>
-
+                        <!--PASSWORD-->
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="col-md-4 control-label">Password</label>
 
@@ -52,7 +66,7 @@
                                 @endif
                             </div>
                         </div>
-
+                        <!--CONFIRM PASSWORD-->
                         <div class="form-group">
                             <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
 
