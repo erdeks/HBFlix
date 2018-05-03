cfdf49401e3ca3d5ec9c051bb6e0ed05bca609c2
jaamito <gm.2012.ian.lopez@gmail.com>
2018-05-03 17:03:51 +0200

    Tabla Users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,17 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('apelligo');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('date');
+            $table->integer('idMensaje');
+            $table->boolean('admin');
+            $table->integer('idFavorito');
             $table->rememberToken();
             $table->timestamps();
+            //$table->foreign('idMensaje')->references('id')->on('mensaje')->onDelete('cascade');
+            //$table->foreign('idFavorito')->references('id')->on('favorito')->onDelete('cascade');
         });
     }
 
