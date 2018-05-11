08ea97480dac87fd3911e2d558d70ac04f3e963b
erdeks <aponcelasgarcia@iesesteveterradas.cat>
2018-05-11 18:27:01 +0200

    menu iniciao

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,29 @@
 @extends('layouts.inicio')
 
 @section('content')
-        <div class="col-md-8">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    You are logged in!
-                    <a href="{{ url('admin') }}">Admin</a>
-                </div>
-            </div>
+      <div class="col-sm-3 col-md-3">
+      </div>
+      <div class="col-sm-9 col-md-9">
+        <div class="panel">
+          <div class="panel-heading" style="background-color: #181E1F;">
+            <h3 class="panel-title" style="color: #EC67A2">Peliculas</h3>
+          </div>
+          <div class="panel-body" style="background-color: #282F30;">
+          <p style="color:  #EC67A2;">
+            <input type="button" value="Editar" onclick="mostrar()" class="btn btn-sm btn-success" style="margin: 5px;">
+          </p>
+         <!--Empieza-->
+         <h1 style="color:  #EC67A2;text-align: center;"><strong>Titulo:</strong> </h1>
+         </br>
+         <ul>
+           <li style="color:  #EC67A2;text-align: center;"><H1>Género:</H1></li>
+           <li style="color:  #EC67A2;text-align: center;"><H1>Año:</H1></li>
+         </ul>
+         <h1 style="color:  #EC67A2;text-align: center;">Resumen</h1>
+         <p style="color:  #EC67A2;text-align: center;"></p>
         </div>
+      </div>
+      </div>
     </div>
 </div>
 
--- resources/views/layouts/inicio.blade.php
+++ resources/views/layouts/inicio.blade.php
@@ -13,191 +13,83 @@
     <style type="text/css">
              body {
                 background-color: #181E1F;
-                border-color: #181E1F;
+                overflow-y: scroll;
              }
-             body{margin:50px;}
-              #accordion .glyphicon { margin-right:10px; }
-              .panel-collapse>.list-group .list-group-item:first-child {border-top-right-radius: 0;border-top-left-radius: 0;}
-              .panel-collapse>.list-group .list-group-item {border-width: 1px 0;}
-              .panel-collapse>.list-group {margin-bottom: 0;}
-              .panel-collapse .list-group-item {border-radius:0;}
-
-              .panel-collapse .list-group .list-group {margin: 0;margin-top: 10px;}
-              .panel-collapse .list-group-item li.list-group-item {margin: 0 -15px;border-top: 1px solid #ddd;border-bottom: 0;padding-left: 30px;}
-              .panel-collapse .list-group-item li.list-group-item:last-child {padding-bottom: 0;}
-
-              .panel-collapse div.list-group div.list-group{margin: 0;}
-              .panel-collapse div.list-group .list-group a.list-group-item {border-top: 1px solid #ddd;border-bottom: 0;padding-left: 30px;}
-
-
+             .link{
+               color: #EC67A2;
+             }
+             .link:hover{
+               color:#EC67A2;
+             }
+             .titulo{
+               text-align: center;
+               font-size: 20px;
+               color: #EC67A2;
+             }
+             .mPos{
+               position: -webkit-sticky;
+               position: sticky;
+               top: 0;
+               left: 0;
+             }
+             .menu{
+               background-color: #282934;
+               height: 800px;
+             }
+             .menu a{
+               color: #ff32e6;
+             }
+             .h5{
+               color: #fa04dc;
+             }
+             .menu-fixed{
+               position:fixed;
+               z-index:1000;
+               top:0;
+               max-width:1000px;
+               width:100%;
+             }
         </style>
     <title>Panel Admin</title>
 </head>
 <body>
-    <nav class="navbar navbar-default navbar-fixed-top" role="navigation" style="background-color: #181E1F; border-radius: 60px;">
     <div class="container">
-      <!-- El logotipo y el icono que despliega el menú se agrupan
-           para mostrarlos mejor en los dispositivos móviles -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse"
-                data-target=".navbar-ex1-collapse">
-          <span class="sr-only">Desplegar navegación</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="#" style="color:  #EC67A2;">Inicio</a>
-      </div>
-     
-      <!-- Agrupar los enlaces de navegación, los formularios y cualquier
-           otro elemento que se pueda ocultar al minimizar la barra -->
-      <div class="collapse navbar-collapse navbar-ex1-collapse">
-        <ul class="nav navbar-nav">
-          <li><a href="#" style="color:  #EC67A2">Enlace #2</a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="color:  #EC67A2">
-              Menú #1 <b class="caret"></b>
-            </a>
-            <ul class="dropdown-menu">
-              <li><a href="#" style="color:  #EC67A2">Acción #1</a></li>
-              <li><a href="#" style="color:  #EC67A2">Acción #2</a></li>
-              <li><a href="#" style="color:  #EC67A2">Acción #3</a></li>
-              <li class="divider"></li>
-              <li><a href="#" style="color:  #EC67A2">Acción #4</a></li>
-              <li class="divider"></li>
-              <li><a href="#" style="color:  #EC67A2">Acción #5</a></li>
-            </ul>
-          </li>
-        </ul>
-     
-        <form class="navbar-form navbar-left" role="search">
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Buscar">
-          </div>
-          <button type="submit" class="btn btn-default">Enviar</button>
-        </form>
-     
-        <ul class="nav navbar-nav navbar-right">
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="color:  #EC67A2">
-               {{ Auth::user()->name }}<b class="caret"></b>
-            </a>
-            <ul class="dropdown-menu">
-                <li>
-                    <a href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                                 document.getElementById('logout-form').submit();" style="color:  #EC67A2">
-                        Logout
-                    </a>
+        <div class="row">
+          <div class="col-sm-3 col-md-3 mPos">
+            <!--MENU USUARIOS-->
+              <img src="{{url('images/logo.png')}}" alt="logo" style="height: 100px; width: 200px;">
+              <div id="sidebar" class="well sidebar-nav menu">
+                  <h4 class="titulo">Informacion Usuarios</h4>
+                  <ul class="nav nav-pills nav-stacked">
+                    <li><a href="#" class="link">&nbsp;{{ Auth::user()->name }}</a></li>
+                    <li><a href="{{ url('/inicio') }}" class="link">&nbsp;Home</a></li>
+                    <li><a href="{{ url('inicio/perfil') }}" class="link">&nbsp;Ver Perfil</a></li>
+                    <li><a href="{{ route('logout') }}" class="link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">&nbsp;Logout
+                    </a></li>
+                  </ul>
+                  <h4 class="panel-title titulo">Generos</h4>
+                  <ul class="nav nav-pills nav-stacked">
+                    @foreach($arrayGenero as $key => $genero)
+                     <li><a href="{{ url('inicio/'.$genero->nombre) }}" class="link">{{$genero->nombre}}</a></li>
+                    @endforeach
+                  </ul>
+                  <h4 class="panel-title titulo">Año Lanzamiento</h4>
+                  <ul class="nav nav-pills nav-stacked">
+                    @foreach($arrayLanz as $key => $aLanz)
+                     <li><a>{{$aLanz->aLanzamiento}}</a></li>
+                    @endforeach
+                  </ul>
+              </div>
 
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </div>
-    </nav>
-    <div class="container " >
-    <img src="{{url('images/logo.png')}}" alt="logo" style="height: 70px;">
-          <div class="row">
-            <div class="col-sm-3 col-md-3">
-              <div class="panel-group" id="accordion">
-              <!--MENU USUARIOS-->
-                <div class="panel panel-default">
-                  <div class="panel-heading" style="background-color: #181E1F;">
-                    <h4 class="panel-title">
-                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" style="color: #EC67A2"><span class="glyphicon glyphicon-folder-close">
-                        </span>&nbsp;Users</a>
-                    </h4>
-                  </div>
-                  <div id="collapseOne" class="panel-collapse collapse">
-                    <ul class="list-group">
-                      <li class="list-group-item" style="background-color: #282F30;"><span class="glyphicon glyphicon-flash text-success"></span><a href="{{ url('/admin/verUsuarios') }}" style="color: #EC67A2">&nbsp;Ver usuarios</a></li>
+            </div>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              {{ csrf_field() }}
+            </form>
 
-                      <li class="list-group-item" style="background-color: #282F30;"><span class="glyphicon glyphicon-file text-info"></span><a href="#" style="color: #EC67A2">&nbsp;Ver usuarios baneados</a></li>
 
-                      <li class="list-group-item" style="background-color: #282F30;"> <span class="glyphicon glyphicon-comment text-success"></span><a href="#" style="color: #EC67A2">&nbsp;Ver mensajes de usuarios</a><span class="badge">42</span></li>
+            @yield('content')
 
-                    </ul>
-                  </div>
-                </div>
-                <!--FINAL MENÚ USUARIO-->
-                <!--MENU PELÍCULAS-->
-                <div class="panel panel-default">
-                  <div class="panel-heading" style="background-color: #181E1F;">
-                    <h4 class="panel-title">
-                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" style="color: #EC67A2"><span class="glyphicon glyphicon-folder-close">
-                        </span>&nbsp;Películas</a>
-                    </h4>
-                  </div>
-                  <div id="collapseTwo" class="panel-collapse collapse">
-                    <ul class="list-group" >
-                      <li class="list-group-item" style="background-color: #282F30;"><span class="glyphicon glyphicon-flash text-success"></span><a href="{{ url('admin/verPeliculas') }}" style="color: #EC67A2">&nbsp;Ver Películas</a></li>
-
-                      <li class="list-group-item" style="background-color: #282F30;"><span class="glyphicon glyphicon-file text-info"></span><a href="{{ url('admin/crearPeliculas') }}" style="color: #EC67A2">&nbsp;Crear película</a></li>
-                    </ul>
-                  </div>
-                </div>
-                <!--FINAL MENÚ PELÍCULAS-->
-                <!--MENU SERIESS-->
-                <div class="panel panel-default">
-                  <div class="panel-heading" style="background-color: #181E1F;">
-                    <h4 class="panel-title">
-                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree" style="color: #EC67A2"><span class="glyphicon glyphicon-folder-close">
-                        </span>&nbsp;Series</a>
-                    </h4>
-                  </div>
-                  <div id="collapseThree" class="panel-collapse collapse">
-                    <ul class="list-group">
-                      <li class="list-group-item" style="background-color: #282F30;"><span class="glyphicon glyphicon-flash text-success"></span><a href="#" style="color: #EC67A2">&nbsp;Ver Series</a></li>
-
-                      <li class="list-group-item" style="background-color: #282F30;"><span class="glyphicon glyphicon-file text-info"></span><a href="#" style="color: #EC67A2">&nbsp;Crear Serie</a></li>
-                    </ul>
-                  </div>
-                </div>
-                <!--FINAL MENÚ SERIES-->
-                <!--MENU GÑÉNERO-->
-                <div class="panel panel-default">
-                  <div class="panel-heading" style="background-color: #181E1F;">
-                    <h4 class="panel-title">
-                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour" style="color: #EC67A2"><span class="glyphicon glyphicon-folder-close">
-                        </span>&nbsp;Género</a>
-                    </h4>
-                  </div>
-                  <div id="collapseFour" class="panel-collapse collapse">
-                    <ul class="list-group">
-                      <li class="list-group-item" style="background-color: #282F30;"><span class="glyphicon glyphicon-flash text-success"></span><a href="#" style="color: #EC67A2">&nbsp;Ver Géneros</a></li>
-
-                      <li class="list-group-item" style="background-color: #282F30;"><span class="glyphicon glyphicon-file text-info"></span><a href="#" style="color: #EC67A2">&nbsp;Crear Género</a></li>
-                    </ul>
-                  </div>
-                </div>
-                <!--FINAL MENÚ GÉNERO-->
-                <!--MENU AÑO DE LANZAMIENTO-->
-                <div class="panel panel-default" >
-                  <div class="panel-heading" style="background-color: #181E1F;">
-                    <h4 class="panel-title" >
-                      <a data-toggle="collapse" data-parent="#accordion" href="#collapseFive" style="color: #EC67A2"><span class="glyphicon glyphicon-folder-close" >
-                        </span>&nbsp;Año de lanzamiento</a>
-                    </h4>
-                  </div>
-                  <div id="collapseFive" class="panel-collapse collapse">
-                    <ul class="list-group">
-                      <li class="list-group-item" style="background-color: #282F30;"><span class="glyphicon glyphicon-flash text-success"></span><a href="#" style="color: #EC67A2">&nbsp;Ver Años de lanzamiento</a></li>
-
-                      <li class="list-group-item" style="background-color: #282F30;"><span class="glyphicon glyphicon-file text-info"></span><a href="#" style="color: #EC67A2">&nbsp;Crear Año de lanzamiento</a></li>
-                    </ul>
-                  </div>
-                </div>
-
-                <!--FINAL MENÚ AÑO DE LANZAMIENTO-->
-         @yield('content')
- </div>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
-
